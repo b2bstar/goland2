f23c06cc7b0e13a8c1416f7355fb2e5b8e279620
b2bstar <33840846+b2bstar@users.noreply.github.com>
2018-04-12 17:52:44 +0200

    Add files via upload

---
+++ Imports.go
@@ -0,0 +1,15 @@
+package main
+
+import( 
+	"fmt"
+	"math"
+	//"math/rand"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.",math.Sqrt(7))
+	//Now you have 2.6457513110645907 problems.
+
+	//fmt.Println("rang 1 to 100",rand.Intn(100))
+	//rang 1 to 100 81
+}
---
+++ packages.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("WOOOOOW",rand.Intn(10))
+}
